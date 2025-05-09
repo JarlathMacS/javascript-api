340a931e490f3aee41a0418db17cc7f796be12a0
Jarlath <jarlath@jarlath.cc>
2025-05-09 00:58:07 -0500

    Add code for detecting a subtle error

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,26 @@ const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal')
 document.getElementById('status').addEventListener('click', (e) => getStatus(e));
 document.getElementById('submit').addEventListener('click', (e) => postForm(e));
 
+function processOptions(form) {
+    let optArray = [];
+
+    for (const element of form.entries()) {
+        if (element[0] === 'options') {
+            optArray.push(element[1]);
+        }
+    }
+    form.delete('options');
+    form.append('options', optArray.join());
+
+    return form;
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById('checksform'));
+    const form = processOptions(new FormData(document.getElementById('checksform')));
+
+    // for (const element of form.entries()) {
+    //     console.log(element);
+    // }
 
     const response = await fetch(API_URL, {
         method: 'POST',
